9417777c73e4b27de46894c464577cd38165b3cb
shota-tanaka-v <shota.tanaka8305@gmail.com>
2025-05-07 14:45:50 +0900

    fix: ios background mode

--- ios/RNAudioRecord.m
+++ ios/RNAudioRecord.m
@@ -28,9 +28,12 @@ RCT_EXPORT_METHOD(init:(NSDictionary *) options) {
 RCT_EXPORT_METHOD(start) {
     RCTLogInfo(@"start");
 
-    // most audio players set session category to "Playback", record won't work in this mode
-    // therefore set session category to "Record" before recording
-    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryRecord error:nil];
+    NSError *error = nil;
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    [session setCategory:AVAudioSessionCategoryPlayAndRecord
+            withOptions:AVAudioSessionCategoryOptionAllowBluetooth | AVAudioSessionCategoryOptionDefaultToSpeaker
+                    error:&error];
+    [session setActive:YES error:&error];
 
     _recordState.mIsRunning = true;
     _recordState.mCurrentPacket = 0;
